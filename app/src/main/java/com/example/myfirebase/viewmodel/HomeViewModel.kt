c31ed3430c40d961ce915527ad6322d968a19952
nisrina salsa <nisrinahikmawan@gmail.com>
2026-01-02 18:56:15 +0700

    menambah Sealed interface untuk definisi status UI pada HomeViewModel.kt

---
+++ app/src/main/java/com/example/myfirebase/viewmodel/HomeViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.myfirebase.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.myfirebase.modeldata.Siswa
+import com.example.myfirebase.repositori.RepositorySiswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+// Sealed interface untuk mendefinisikan status UI
+sealed interface StatusUiSiswa {
+    data class Success(val siswa: List<Siswa> = listOf()) : StatusUiSiswa
+    object Error : StatusUiSiswa
+    object Loading : StatusUiSiswa
+}
+
