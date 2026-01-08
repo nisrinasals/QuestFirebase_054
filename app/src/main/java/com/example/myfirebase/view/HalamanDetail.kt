f2cc7da763638fb5b2dd489d1ff8f50bab409f1f
nisrina salsa <nisrinahikmawan@gmail.com>
2026-01-08 18:59:28 +0700

    membuat isi scaffold (innerPadding: BodyDetailSiswa) pada fun DetailSiswaScreen

--- app/src/main/java/com/example/myfirebase/view/HalamanDetail.kt
+++ app/src/main/java/com/example/myfirebase/view/HalamanDetail.kt
@@ -1,12 +1,15 @@
 package com.example.myfirebase.view
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
@@ -16,6 +19,7 @@ import com.example.myfirebase.view.route.DestinasiDetail
 import com.example.myfirebase.viewmodel.DetailViewModel
 import com.example.myfirebase.viewmodel.PenyediaViewModel
 import com.example.myfirebase.viewmodel.StatusUIDetail
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -51,6 +55,18 @@ fun DetailSiswaScreen(
             }
         }, modifier = modifier
     ) { innerPadding ->
-
+        val coroutineScope = rememberCoroutineScope()
+        BodyDetailSiswa(
+            statusUIDetail = viewModel.statusUIDetail,
+            onDelete = {
+                coroutineScope.launch {
+                    viewModel.hapusSatuSiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+        )
     }
 }
