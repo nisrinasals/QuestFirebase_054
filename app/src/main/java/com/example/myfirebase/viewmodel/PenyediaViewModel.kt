34a4271eb5c5f5c794e2e8d157d7a684bcd0d0a9
nisrina salsa <nisrinahikmawan@gmail.com>
2026-01-08 19:23:58 +0700

    menambah initializer detail dan edit untuk factory vm

--- app/src/main/java/com/example/myfirebase/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/PenyediaViewModel.kt
@@ -2,6 +2,7 @@ package com.example.myfirebase.viewmodel
 
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -15,5 +16,8 @@ object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { HomeViewModel(aplikasiDataSiswa().container.repositorySiswa) }
         initializer { EntryViewModel(aplikasiDataSiswa().container.repositorySiswa) }
+        initializer { DetailViewModel (this.createSavedStateHandle(), aplikasiDataSiswa().container.repositorySiswa) }
+        initializer { EditViewModel (this.createSavedStateHandle(), aplikasiDataSiswa().container.repositorySiswa) }
+
     }
 }
