573669f2d15441931ec1526e5266a0f199e373a4
nisrina salsa <nisrinahikmawan@gmail.com>
2026-01-08 18:49:41 +0700

    buat fun getSatuSiswa class DetailViewModel

--- app/src/main/java/com/example/myfirebase/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/DetailViewModel.kt
@@ -33,5 +33,18 @@ class DetailViewModel(
         getSatuSiswa()
     }
 
-    
+    fun getSatuSiswa() {
+        viewModelScope.launch {
+            statusUIDetail = StatusUIDetail.Loading
+            statusUIDetail = try {
+                StatusUIDetail.Success(satusiswa = repositorySiswa.getSatuSiswa(idSiswa))
+            } catch (e: IOException) {
+                StatusUIDetail.Error
+            } catch (e: Exception) {
+                StatusUIDetail.Error
+            }
+        }
+    }
+
+
 }
